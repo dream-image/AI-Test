560094cd8e2f07fed8fb43b24c399de96157f834
ThousandDream <2392492803@qq.com>
2026-01-02 03:35:11 +0800

    feat: Enable HTTPS dev server with `vite-plugin-mkcert`, add origin trial meta tags, and implement multi-modal chat input with image support in chrome and the Edge not supports it.

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,11 +2,17 @@ import { useEffect, useRef, useState } from 'react'
 
 import './App.css'
 import { Button, Card, CardBody, CardFooter, Spinner, Textarea } from '@heroui/react'
-import { useAsyncEffect, useMount } from 'ahooks'
+import { useAsyncEffect, useMemoizedFn, useMount } from 'ahooks'
+import { isString } from 'es-toolkit'
 
 const createSession = async () => {
   const controller = new AbortController()
   const session = await LanguageModel.create({
+    expectedInputs: [
+      { type: "text" },
+      // { type: "audio" },
+      // { type: "image" },
+    ],
     monitor(m: any) {
       m.addEventListener('downloadprogress', (e: any) => {
         console.log(`Downloaded ${e.loaded * 100}%`);
@@ -26,26 +32,69 @@ function App() {
   const [text, setText] = useState("")
   const [message, setMessage] = useState<{
     role: 'user' | 'assistant' | 'system',
-    content: string
+    content: string | {
+      type: 'image',
+      value: Blob
+    }[]
   }[]>([
-    {
-      role: "system",
-      content: `
-你是一位精通中文的语言润色专家，擅长将文本润色为优美、流畅且富有诗意的中文表达。请遵循以下要求：
+    //     {
+    //       role: "system",
+    //       content: `
+    // 你是一位精通中文的语言润色专家，擅长将文本润色为优美、流畅且富有诗意的中文表达。请遵循以下要求：
 
-若输入文本为纯中文，则输出必须为中文，并保持文意不变；
+    // 若输入文本为纯中文，则输出必须为中文，并保持文意不变；
 
-对文本进行措辞优化、句式调整，带有诗词的对仗和工整；
+    // 对文本进行措辞优化、句式调整，带有诗词的对仗和工整；
 
-不添加额外解释，直接输出润色后的完整文本。
-      `
-    },
+    // 不添加额外解释，直接输出润色后的完整文本。
+    //       `
+    //     },
     {
       role: "assistant",
       content: "我是你的小助手，有什么需要帮忙的？"
     }
   ])
+  const sendMessage = useMemoizedFn(async () => {
+    const _message = message.concat({
+      role: 'user',
+      content: text
+    })
+    setText('')
+    setMessage(_message);
+    console.log('@@run@@', _message);
+    ; (async () => {
+      console.log('run', _message);
+
+      const stream = sessionRef.current.promptStreaming(_message)
+
+      _message.push({
+        role: 'assistant',
+        content: ""
+      })
+      setLoading(true)
+      for await (const chunk of stream) {
+        _message[_message.length - 1].content += chunk
+        console.log('chunk', chunk);
+
+        setMessage(_message)
+      }
+      setLoading(false)
+    })()
+  })
   const init = async () => {
+    const image = await import('../public/猫羽雫1.png')
+    const fileBlob = await (await fetch(image.default)).blob()
+    console.log('fileBlob', fileBlob);
+
+    setMessage(message.concat({
+      role: 'user',
+      content: [
+        {
+          'type': 'image',
+          value: fileBlob
+        }
+      ]
+    }))
     const res = await LanguageModel.availability({ languages: ["en", "ja"] });
     console.log(res)
     const { session, controller } = await createSession()
@@ -62,7 +111,7 @@ function App() {
         <CardBody className=''>
           {
             message?.filter(i => i.role !== 'system')?.map?.((i, index) => {
-              return <div key={index} className={`${i.role === 'user' ? ' text-end' : ''} w-full `}>{i.content}</div>
+              return <div key={index} className={`${i.role === 'user' ? ' text-end' : ''} w-full `}>{isString(i.content) ? i.content : ''}</div>
             })
           }
         </CardBody>
@@ -83,31 +132,15 @@ function App() {
             }}
             placeholder="请输入"
             variant="bordered"
-          />
-          <Button isLoading={loading} color="primary" onPress={() => {
-            const _message = message.concat({
-              role: 'user',
-              content: text
-            })
-            setText('')
-            setMessage(_message);
-            ; (async () => {
-              const stream = await sessionRef.current.promptStreaming(_message)
-
-              _message.push({
-                role: 'assistant',
-                content: ""
-              })
-              setLoading(true)
-              for await (const chunk of stream) {
-                _message[_message.length - 1].content += chunk
-                console.log('chunk', chunk);
-
-                setMessage(_message)
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                e.preventDefault()
+                e.stopPropagation()
+                sendMessage()
               }
-              setLoading(false)
-            })()
-          }}>确定</Button>
+            }}
+          />
+          <Button isLoading={loading} color="primary" onPress={sendMessage}>确定</Button>
         </div>
       }
 
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    https: {},
+    port: 443,
+  },
   plugins: [
     react({
       babel: {
@@ -11,5 +17,6 @@ export default defineConfig({
       },
     }),
     tailwindcss(),
+    mkcert(),
   ],
 })
